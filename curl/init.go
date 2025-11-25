2123be28c4714cb788afc4c4681bb3c1b8524cc2
shishkin@zarya.ru <shishkin@zarya.ru>
2025-11-25 13:48:14 +0300

    Update lib curl. Add method CurlWithContext

--- curl/init.go
+++ curl/init.go
@@ -1,6 +1,7 @@
 package curl
 
 import (
+	"context"
 	"io"
 	"net/http"
 )
@@ -33,6 +34,26 @@ func Curl(
 		return nil, 0, err
 	}
 
+	return body, resp.StatusCode, nil
+}
+
+func CurlWithContext(ctx context.Context, client *http.Client, url string, method string, bodyReader io.Reader, headers map[string]string) ([]byte, int, error) {
+	req, err := http.NewRequestWithContext(ctx, method, url, bodyReader)
+	if err != nil {
+		return nil, 0, err
+	}
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer clearResponse(resp)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, 0, err
+	}
 	return body, resp.StatusCode, nil
 }
 
