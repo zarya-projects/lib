d0f15970931b97360ec972737e21f907f6460c2f
shishkin@zarya.ru <shishkin@zarya.ru>
2025-11-26 11:48:16 +0300

    Update 1

--- redis/init.go
+++ redis/init.go
@@ -20,6 +20,7 @@ type Options struct {
 type SRedis struct {
 	client *redis.Client
 	logger *slog.Logger
+	opts   Options
 }
 
 type Base interface {
@@ -28,34 +29,38 @@ type Base interface {
 	HealthCheck() error
 }
 
-func (r *SRedis) Connect(opts Options) error {
+func New(opts Options, logger *slog.Logger) *SRedis {
+	return &SRedis{opts: opts, logger: logger}
+}
+
+func (r *SRedis) Connect() error {
 	var err error
 
 	switch {
-	case opts.Addr == "":
+	case r.opts.Addr == "":
 		return errors.New("redis: addr is required")
-	case opts.Password == "":
+	case r.opts.Password == "":
 		return errors.New("redis: password is required")
-	case opts.DB == 0:
+	case r.opts.DB == 0:
 		return errors.New("redis: db is required")
 	}
 
-	if opts.MaxRetries == 0 {
-		opts.MaxRetries = 5
+	if r.opts.MaxRetries == 0 {
+		r.opts.MaxRetries = 5
 	}
-	if opts.TimeRetry == 0 {
-		opts.TimeRetry = 5 * time.Second
+	if r.opts.TimeRetry == 0 {
+		r.opts.TimeRetry = 5 * time.Second
 	}
 
-	for i := 0; i < opts.MaxRetries; i++ {
+	for i := 0; i < r.opts.MaxRetries; i++ {
 		r.client = redis.NewClient(&redis.Options{
-			Addr:     opts.Addr,
-			Password: opts.Password,
-			DB:       opts.DB,
+			Addr:     r.opts.Addr,
+			Password: r.opts.Password,
+			DB:       r.opts.DB,
 		})
 
 		if err = r.client.Ping(context.Background()).Err(); err != nil {
-			time.Sleep(opts.TimeRetry)
+			time.Sleep(r.opts.TimeRetry)
 			continue
 		}
 
